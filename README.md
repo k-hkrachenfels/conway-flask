aa77bbba18e76a3a4d33e4927b6b158372f2cecd
Karl-Heinz Krachenfels <karl-heinz@krachenfels.de>
2022-02-12 19:50:41 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notes
+
+## How to start
+
+Run with: 
+```
+python -m flask run
+```
+
+## How to run
+build and run the docker container 
---
+++ app.py
@@ -0,0 +1,135 @@
+from flask import Flask
+from datetime import datetime
+import re
+import numpy as np
+import scipy.signal
+import matplotlib
+import matplotlib.pylab as plt
+import matplotlib.animation
+
+
+app = Flask(__name__)
+
+# initial configuration for glider
+A = np.array([
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,1,0,0,0,0],
+  [0,0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0]])
+
+A = np.array([
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])  
+
+# kernel
+K = np.asarray([
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]])
+
+def figure_asset(K, growth):
+  K_sum = np.sum(K)
+  K_size = K.shape[0]
+  K_mid = K_size // 2
+
+  matplotlib.use('agg')
+  fig, ax = plt.subplots(1, 3, figsize=(14, 2), gridspec_kw={'width_ratios': [1,1,2]})
+
+  ax[0].imshow(K, cmap="jet", interpolation="nearest", vmin=0)
+  ax[0].title.set_text('kernel K')
+
+  ax[1].bar(range(K_size), K[K_mid,:], width=1)
+  ax[1].title.set_text('Kernel K cross-section')
+  ax[1].set_xlim([K_mid - 1 - 3, K_mid + 1 + 3])
+
+  x = np.arange(K_sum + 1)
+  ax[2].step(x, growth(x))
+  ax[2].axhline(y=0, color='grey', linestyle='dotted')
+  ax[2].title.set_text('growth G')
+  return fig
+
+def figure_world(A):
+  global img
+  fig = plt.figure(figsize=(15,15))
+  img = plt.imshow(A, cmap="jet", interpolation="nearest", vmin=0)
+  plt.title('World A')
+  plt.close()
+  return fig
+
+def growth(U):
+  return 0 + (U==3) - ((U<2)|(U>3))
+
+def evolve(i):
+  global A
+  U = scipy.signal.convolve2d(A, K, mode='same', boundary='wrap')
+  A = np.clip(A + growth(U), 0, 1)
+  img.set_array(A)
+  return img,
+
+def get_video():
+    # render kernel and growth
+    figure_asset(K, growth)
+
+    # render video
+    fig = figure_world(A)
+    anim = matplotlib.animation.FuncAnimation(fig, evolve, frames=150, interval=500)
+    return anim.to_html5_video()
+
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    html= get_video()
+    print(html)
+    return html
+
